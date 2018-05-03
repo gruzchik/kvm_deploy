e57669ad54d53fdfbd4b323a40ef6a612b59975c
Pro <ahtyrka@gmail.com>
2018-05-03 10:31:20 +0300

    add image creating

--- task10_12_1.sh
+++ task10_12_1.sh
@@ -8,5 +8,21 @@ source ${SCRIPTPATH}/config
 echo vm1=${VM1_NAME}
 echo vm2=${VM2_NAME}
 
+# create disk for vm1
+VM1_HDD_PATH=$(dirname ${VM1_HDD})
+[ ! -d ${VM1_HDD_PATH} ] && mkdir -p ${VM1_HDD_PATH}
 echo "qemu-img create -f qcow2 ${VM1_HDD} 5Gb"
 qemu-img create -f qcow2 ${VM1_HDD} 5G
+
+## create vm1
+#virt-install \
+#--connect qemu:///system \
+#--name ${VM1_NAME} \
+#--ram ${VM1_MB_RAM} --vcpus=${VM1_NUM_CPU} --hvm \
+#--os-type=linux --os-variant=ubuntu16.04 \
+#--disk path=${VM1_HDD},format=qcow2,bus=virtio,cache=none \
+#--location 'http://us.archive.ubuntu.com/ubuntu/dists/xenial/main/installer-amd64/' \
+#--graphics vnc,port=-1 \
+#--noautoconsole --quiet --virt-type kvm --import
+##--disk path=${VM1_CONFIG_ISO},device=cdrom \
+##--network network=default,mac='52:54:00:07:ca:ba' \
