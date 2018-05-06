f07907c6502e2fff7fef9f8bcb52deaf33cbf2e2
Pro <i.pogorelko@gmail.com>
2018-05-07 02:17:48 +0300

    add network interfaces

--- task10_12_1.sh
+++ task10_12_1.sh
@@ -8,6 +8,52 @@ source ${SCRIPTPATH}/config
 echo vm1=${VM1_NAME}
 echo vm2=${VM2_NAME}
 
+#create xml external network and start net
+MAC=52:54:00:`(date; cat /proc/interrupts) | md5sum | sed -r 's/^(.{6}).*$/\1/; s/([0-9a-f]{2})/\1:/g; s/:$//;'`
+cat <<EOF > ${SCRIPTPATH}/networks/external.xml
+<network>
+    <name>${EXTERNAL_NET_NAME}</name>
+    <forward mode='nat'>
+      <nat>
+        <port start='1024' end='65535'/>
+      </nat>
+    </forward>
+    <ip address='${EXTERNAL_NET_HOST_IP}' netmask='${EXTERNAL_NET_MASK}'>
+      <dhcp>
+        <range start='${EXTERNAL_NET}.2' end='${EXTERNAL_NET}.254' />
+        <host mac='${MAC}' name='${VM1_NAME}' ip='${VM1_EXTERNAL_IP}'/>
+      </dhcp>
+    </ip>
+  </network>
+EOF
+
+virsh net-define ${SCRIPTPATH}/networks/external.xml
+#virsh net-create ${EXTERNAL_NET_NAME}
+virsh net-autostart ${EXTERNAL_NET_NAME}
+virsh net-start ${EXTERNAL_NET_NAME}
+
+#create xml internal network and start net
+cat <<EOF > ${SCRIPTPATH}/networks/internal.xml
+<network>
+    <name>${INTERNAL_NET_NAME}</name>
+  </network>
+EOF
+
+virsh net-define ${SCRIPTPATH}/networks/internal.xml
+virsh net-autostart ${INTERNAL_NET_NAME}
+virsh net-start ${INTERNAL_NET_NAME}
+
+#create xml management network and start net
+cat <<EOF > ${SCRIPTPATH}/networks/management.xml
+<network>
+    <name>${MANAGEMENT_NET_NAME}</name>
+  </network>
+EOF
+
+virsh net-define ${SCRIPTPATH}/networks/management.xml
+virsh net-autostart ${MANAGEMENT_NET_NAME}
+virsh net-start ${MANAGEMENT_NET_NAME}
+
 # create disk for vm1
 VM1_HDD_PATH=$(dirname ${VM1_HDD})
 [ ! -d ${VM1_HDD_PATH} ] && mkdir -p ${VM1_HDD_PATH}
